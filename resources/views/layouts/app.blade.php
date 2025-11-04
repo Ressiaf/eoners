e369740bbe95b73f7ada3549db6006023d125d2a
Ressiaf <facurd91@gmail.com>
2025-11-03 22:18:24 -0300

    Tailwindcss config

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Eoners - @yield('title')</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        <h1 class="text-4xl font-bold text-center mt-20">
+         Tailwind v4 funcionando en Laravel
+        </h1>
+    </h1>
+    </body>
+</html>
